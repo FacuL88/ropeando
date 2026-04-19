6bc64d0a8ee9ddae9902773a6bb4ab0d92e90433
FacuL88 <lauglefacundo@gmail.com>
2026-04-18 22:08:44 -0300

    Finalizacion de configuracion

--- app/src/provider.js
+++ app/src/provider.js
@@ -1,6 +1,9 @@
-// Se mantienen los imports de las imágenes...
+import prenda_01 from './static/images/book/bebe/prenda_01.jpg';
 import prenda_02 from './static/images/book/bebe/prenda_02.jpg';
-// ... (todos tus otros imports)
+import prenda_03 from './static/images/book/bebe/prenda_03.jpg';
+import prenda_04 from './static/images/book/bebe/prenda_04.jpg';
+import prenda_05 from './static/images/book/bebe/prenda_05.jpg';
+import prenda_06 from './static/images/book/bebe/prenda_06.jpg';
 
 export const items = [
     {
@@ -8,22 +11,41 @@ export const items = [
         name: 'Conjunto Algodón Bebe',
         desc: 'Material orgánico, ideal para recién nacidos.',
         price: 35000,
-        img: prenda_02
+        img: prenda_01
     },
     {
         id: 2,
         name: 'Body Estampado',
         desc: 'Talle M - 100% Algodón.',
         price: 32000,
-        img: prenda_03
+        img: prenda_02
     },
-    // Aplica nombres únicos a tus productos para que el carrito se vea real
     {
         id: 3,
         name: 'Enterito de Lana',
         desc: 'Tejido artesanal premium.',
         price: 45000,
+        img: prenda_03
+    },
+    {
+        id: 4,
+        name: 'Pijama Suave',
+        desc: 'Perfecto para dormir cómodo.',
+        price: 28000,
         img: prenda_04
     },
-    // ... continuar con el resto de los items
+    {
+        id: 5,
+        name: 'Vestido Verano',
+        desc: 'Ligero y fresco para días calurosos.',
+        price: 38000,
+        img: prenda_05
+    },
+    {
+        id: 6,
+        name: 'Conjunto Invierno',
+        desc: 'Abrigo y gorro incluidos.',
+        price: 52000,
+        img: prenda_06
+    }
 ];
